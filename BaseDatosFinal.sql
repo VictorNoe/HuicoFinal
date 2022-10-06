6fd0b8a56c202f08a7cf0c41dd3864e8f6912a27
VictorNoe <20213tn059@utez.edu.mx>
2022-10-06 10:07:54 -0500

    base de datos

---
+++ BaseDatosFinal.sql
@@ -0,0 +1,19 @@
+create database encuesta;
+use encuesta;
+drop table personas;
+CREATE TABLE `encuesta`.`personas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(150) NULL,
+  `Escuela` VARCHAR(150) NULL,
+  `Nombre_Encuesta` VARCHAR(150) NULL,
+  `pregunta1` int(10) NULL,
+  `pregunta2` int(10) NULL,
+  `pregunta3` int(10) NULL,
+  `pregunta4` int(10) NULL,
+  `pregunta5` int(10) NULL,
+  `pregunta6` int(10) NULL,
+  `pregunta7` int(10) NULL,
+  `pregunta8` int(10) NULL,
+  `total` double NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
